390c3ed9e50713d4fe918f9a94e1d81018f1058b
Jane Lutak <foux02911@gmail.com>
2023-11-03 07:52:14 +0300

    Вывели на экран в модальном окно

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ let screenPrice = 16390;
 let rollback = 4000;
 let fullPrice = screenPrice + rollback;
 let adaptive = "Подойдет для тех, кто заботиться о своем зрении";
+
+alert(
+  "Представляем вашему вниманию " +
+    TITLE +
+    ". " +
+    "C форматом экрана " +
+    SCREENS +
+    ". " +
+    "По скромной цене: " +
+    fullPrice +
+    " руб."
+);
