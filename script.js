192817cfa38569401ee1e9b9577ba573fe92b195
Jane Lutak <foux02911@gmail.com>
2023-11-03 22:41:44 +0300

    Переменные все константы. Применила интерполяцию выражений

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-const TITLE = "Philips 272V8LA";
-const SCREENS = "16:9";
-let screenPrice = 16390;
-let rollback = 4000;
-let fullPrice = screenPrice + rollback;
-let adaptive = "Подойдет для тех, кто заботиться о своем зрении";
+const title = "Philips 272V8LA";
+const screens = "16:9";
+const screenPrice = 16390;
+const rollback = 4000;
+const fullPrice = screenPrice + rollback;
+const adaptive = "Подойдет для тех, кто заботиться о своем зрении";
 
 alert(
-  "Представляем вашему вниманию " +
-    TITLE +
-    ". " +
-    "C форматом экрана " +
-    SCREENS +
-    ". " +
-    "По скромной цене: " +
-    fullPrice +
-    " руб."
+  `Представляем вашему вниманию ${title}. C форматом экрана ${screens}. По скромной цене: ${fullPrice} руб.`
 );
 
-console.log("Купите, не пожалеете! PS: " + adaptive + ".");
+console.log(`Купите, не пожалеете! PS: ${adaptive}.`);
