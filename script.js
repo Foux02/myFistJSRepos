0d436f8e7ececb6252ae11c655a2e4cc2b22e7e9
Jane Lutak <foux02911@gmail.com>
2023-11-03 07:48:29 +0300

    Создали переменные

---
+++ script.js
@@ -0,0 +1,6 @@
+const TITLE = "Philips 272V8LA";
+const SCREENS = "16:9";
+let screenPrice = 16390;
+let rollback = 4000;
+let fullPrice = screenPrice + rollback;
+let adaptive = "Подойдет для тех, кто заботиться о своем зрении";
