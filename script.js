1a1d7b549d5a5aee2d60cd68cc6887886694d9f4
Jane Lutak <foux02911@gmail.com>
2023-11-05 01:38:17 +0300

    Задание урока 2 (простое) выполнено

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const title = "Philips 272V8LA";
-const screens = "16:9";
-const screenPrice = 16390;
-const rollback = 4000;
-const fullPrice = screenPrice + rollback;
-const adaptive = "Подойдет для тех, кто заботиться о своем зрении";
+const title = "Проект: Зоомагазин Ёж";
+let screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 500;
+const rollback = 50;
+const fullPrice = 1000;
+const adaptive = true;
 
 alert(
   `Представляем вашему вниманию ${title}. C форматом экрана ${screens}. По скромной цене: ${fullPrice} руб.`
 );
 
 console.log(`Купите, не пожалеете! PS: ${adaptive}.`);
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов: ${screenPrice} долларов.`);
+console.log(`Стоимость разработки сайта: ${fullPrice} долларов.`);
+
+screens = screens.toLowerCase();
+
+console.log(screens.split(","));
+console.log(fullPrice * (rollback / 100));
