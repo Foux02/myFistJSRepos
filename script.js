6c49adeef7eca84660f04cb61e39048ecf46a650
Jane Lutak <foux02911@gmail.com>
2023-11-06 23:54:37 +0300

    Выполнены пункты задания с 1 по 6

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const title = "Проект: Зоомагазин Ёж";
+let title = "Проект: Зоомагазин Ёж";
 let screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 500;
+let screenPrice = 500;
 const rollback = 50;
 const fullPrice = 1000;
-const adaptive = true;
+let adaptive = true;
 
 alert(
   `Представляем вашему вниманию ${title}. C форматом экрана ${screens}. По скромной цене: ${fullPrice} руб.`
@@ -21,3 +21,18 @@ console.log(`Стоимость разработки сайта: ${fullPrice} д
 
 console.log(screens.toLowerCase().split(", "));
 console.log(fullPrice * (rollback / 100));
+
+//Урок №3
+
+title = prompt("Как называется ваш проект?");
+screens = prompt(
+  "Какие типы экранов нужно разработать?",
+  "Простые, Сложные, Интерактивные"
+);
+screenPrice = parseFloat(
+  prompt("Сколько будет стоить данная работа?").replace(",", `.`)
+);
+
+adaptive = confirm("Нужен ли адаптив на сайте?");
+
+console.log(adaptive);
